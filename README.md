6bd4899391d0d992d7019067162f0b630a5cf688
Manuel Shenavai <manuel.shenavai@sap.com>
2021-04-21 22:29:24 +0200

    Readme updated to explain how to reproduce Comments removed

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # CAMEL-16532
 Camel CXF: Trying to start already registered endpoint, will unregister existing one
 1) Run DemoApplication to startup the tomcat and camel context
-2) Do a POST request to http://127.0.0.1:9900/cxf/manuel with following payload. Response should be HTTP 200 (response body=FIRST)
+2) Do a POST request to http://127.0.0.1:9900/cxf/manuel with following payload. Response should be HTTP 200 (response body comes from Route1: FIRST)<br>
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><n2:invoke xmlns:n2="http://cxf.component.camel.apache.org/">test</n2:invoke></soap:Body></soap:Envelope>
 
-3) Do GET request to http://localhost:9900/route/start to start camel route with a CXF endpoint that is already registered. HTTP 500 is expected. This will unregister the existing endpoint (unexpected behavior)
+3) Do GET request to http://localhost:9900/route/start to start Route2 with a CXF endpoint that is already registered. HTTP 500 is expected. This will unregister the existing endpoint from Route1 (unexpected behavior)
 4) Do POST request from step 2. Now a HTTP 404 is returned (the endpoint has been unregistered :()
-5) Do GET request from step 3 to start the route again. This time it will succeeed HTTP 200. The second route is started and the previously running route is gone. (response body=SECOND)
-6) do POST request from step 2) and get HTTP 200 response
+5) Do GET request from step 3 to start the Route2 again. This time it will succeeed HTTP 200. Route2 is started
+6) Do POST request from step 2) and get HTTP 200 response (response body comes from Route2: SECOND)
+
+
+<br>
+Expected behavior: After step3, step4 should still succeed and return HTTP 200 with response from Route1 (FIRST)
--- src/main/java/de/mash/demo/MyAppConfig.java
+++ src/main/java/de/mash/demo/MyAppConfig.java
@@ -1,79 +1,16 @@
 package de.mash.demo;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.RoutesBuilder;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.cxf.CxfEndpoint;
-import org.apache.camel.component.cxf.DataFormat;
-import org.apache.cxf.Bus;
-import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.transport.servlet.CXFServlet;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyAppConfig {
-    //https://camel.apache.org/camel-spring-boot/3.7.x/spring-boot.html
-    //    @Autowired
-    //    CamelContext camelContext;
 
     @Bean
     public ServletRegistrationBean disServlet() {
         return new ServletRegistrationBean(new CXFServlet(), "/cxf/*");
     }
-
-//    @Autowired
-//    private Bus bus;
-//
-//    @Bean(name = Bus.DEFAULT_BUS_ID)
-//    public Bus springBus() {
-//        return new SpringBus();
-//    }
-//
-//    @Bean
-//    public CxfEndpoint customerServiceEndpoint() {
-//        CxfEndpoint cxfEndpoint = new CxfEndpoint();
-////        cxfEndpoint.setAddress("http://127.0.0.1:8080/cxf/manuel");
-//        cxfEndpoint.setAddress("/test");
-//        cxfEndpoint.setBus(bus);
-//        cxfEndpoint.setDataFormat(DataFormat.PAYLOAD);
-//        return cxfEndpoint;
-//    }
-//
-//    @Bean
-//    RoutesBuilder first() {
-//        return new RouteBuilder() {
-//            @Autowired
-//            private CxfEndpoint endpoint;
-//
-//            @Override
-//            public void configure() throws Exception {
-//                String url = endpoint.getPublishedEndpointUrl();
-//                System.out.println(url);
-//                from(endpoint).routeId("eager").bean(MyBean.class, "saySomething()").to("mock:result");
-////                return new RouteBuilder() {
-////                    @Override
-////                    public void configure() throws Exception {
-////                        from("timer:hello?period=1000").routeId("eager").bean(MyBean.class, "saySomething()").to("mock:result");
-//                System.out.println(endpoint.getPublishedEndpointUrl());
-//                System.out.println(endpoint.getPublishedEndpointUrl());
-//            }
-//
-//        };
-//    }
-//
-//    @Bean
-//    RoutesBuilder second() {
-//        return new RouteBuilder() {
-//
-//            @Override
-//            public void configure() throws Exception {
-//                from("timer:hello?period=100").routeId("lazy").noAutoStartup().bean(MyBean.class, "saySomething()").to("mock:result");
-//            }
-//
-//        };
-//    }
 
 }
